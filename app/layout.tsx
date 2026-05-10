2eb861b88dfa127678e37961e00f9f22106ff0f9
Becii <wallnerebecca@gmail.com>
2026-05-10 13:09:12 +0200

    changed lang to de-AT

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${redHat.variable} ${tanMonCheri.variable} h-full antialiased`}>
+    <html lang="de-AT" className={`${redHat.variable} ${tanMonCheri.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <ClientLayout>{children}</ClientLayout>
       </body>
